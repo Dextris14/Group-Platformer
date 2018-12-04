8724a1ba7615c4851e0a117cb2504ad46f3c3269
Alejandro Magana <20.alejandro.magana@ksd.org>
2018-12-04 08:32:13 -0800

    Adding functions to buttons

---
+++ Platformer Project/Assets/Scenes/ScriptsforPauseandMenu/MenuButtons.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuButtons : MonoBehaviour {
+
+	public void NewGame()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Main Menu");
+    }
+}
