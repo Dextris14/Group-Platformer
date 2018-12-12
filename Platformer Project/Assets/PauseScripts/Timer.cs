844b2dcfb6bfda062318cd96dff17f1ace979f99
Mason Winward <17.mason.winward@ksd.org>
2018-12-12 10:06:39 -0800

    Level 1 Timer for Reference to other Levels

---
+++ Platformer Project/Assets/PauseScripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class Timer : MonoBehaviour {
+
+    public Text TimeLeft;
+    float timer = 75;
+    public Slider Slider;
+
+    // Update is called once per frame
+    void Update () {
+        timer -= Time.deltaTime;
+        TimeLeft.GetComponent<Text>().text = "" + Mathf.RoundToInt(timer) + "/75";
+        Slider.GetComponent<Slider>().value = timer;
+        if (timer <= 0)
+        {
+            SceneManager.LoadScene("Lose");
+        }
+
+    }
+}
