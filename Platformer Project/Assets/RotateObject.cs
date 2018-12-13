a9cd33aeeaaf1e3b2cc14a242b38b3d6f7fae330
Mason Winward <17.mason.winward@ksd.org>
2018-12-13 09:29:16 -0800

    Pause Rotation

--- Platformer Project/Assets/RotateObject.cs
+++ Platformer Project/Assets/RotateObject.cs
@@ -12,6 +12,10 @@ public class RotateObject : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 0, spinSpeed, Space.World);
+        if (Time.timeScale == 1)
+        { transform.Rotate(0, 0, spinSpeed, Space.World);
+        }
+        
+
 	}
 }
